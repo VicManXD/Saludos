b0b5065436ff9c20d0b5d5c98d276eaa9d7127b5
Victor <vicman400y@gmail.com>
2021-11-05 23:25:11 -0400

    Se agrega el Proyecto

---
+++ src/Saludos/Saludos.java
@@ -0,0 +1,22 @@
+package Saludos;
+
+public class Saludos {
+
+	public static void main(String[] args) {
+
+		System.out.println("------------------------------------------");
+		System.out.println("|                                        |");
+		System.out.println("| SALUDOS INGE YA APRENDÍ A USAR GUIT :D |");
+		System.out.println("|                                        |");
+		System.out.println("|   Deme un poco mas tiempo por favor    |");
+		System.out.println("| 	 estoy viendo las clases todavía     |");
+		System.out.println("| 	       para guiarme mejor.           |");
+		System.out.println("|                                        |");
+		System.out.println("|     Atte: Victor Manuel Yugar Vega     |");
+		System.out.println("|                                        |");
+		System.out.println("------------------------------------------");
+		
+
+	}
+
+}
